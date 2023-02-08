09855353543fe551c4c413fcc988569e47209809
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-02-08 23:56:10 +0100

    ♟ Added mobile

--- script/chessboard.js
+++ script/chessboard.js
@@ -1,4 +1,4 @@
-var u = 50 / 2;
+var u = 75 / 2;
 
 var chessboard = [];
 
@@ -10,6 +10,8 @@ var indexX, indexY;
 var startX, startY;     // TO SEND AS START POS
 var currentX, currentY; // TO SEND AS FINAL POS
 
+var lastMoveX = -1, lastMoveY = -1;
+
 var activePiece, markedPiece;
 
 var warnCases = [];
@@ -18,9 +20,10 @@ var move = true;
 
 function focusPiece(event) {
     if (
-    (move && (Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0))
+    ((move && (Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0))
     || 
-    (!move && !(Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0))
+    (!move && !(Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0)))
+    && event.which == 1
     ) {
         if (markedPiece)
             markedPiece.style.boxShadow =  '';
@@ -42,27 +45,64 @@ function focusPiece(event) {
         // call function get moves
     }
 }
-function warnCase(event) {
-    if (event.which == 3) {
-        let target = event.target;
-        if (event.target.tagName == 'DIV')
-            target = target.parentElement;
+function mobileFocusPiece(event) {
+    if (
+        ((move && (Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0))
+        || 
+        (!move && !(Array.prototype.indexOf.call(event.target.classList, 'wr') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wn') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wb') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wq') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wk') >= 0 || Array.prototype.indexOf.call(event.target.classList, 'wp') >= 0)))
+        ) {
+            if (markedPiece)
+                markedPiece.style.boxShadow =  '';
+    
+            activePiece = event.target;
+    
+            mouseX = event.touches[0].clientX;
+            mouseY = event.touches[0].clientY;
 
-        let targetIndex = Array.prototype.indexOf.call(warnCases, target);
-        if (targetIndex >= 0) {
-            warnCases.splice(targetIndex, 1);
-            target.style.background = ''
-        }
-        else {
-            target.style.background = '#db4e2c'
-
-            let X = Array.prototype.indexOf.call(target.parentNode.children, target) - 1;
-            let Y = Math.abs(Array.prototype.indexOf.call(target.parentNode.parentNode.children, target.parentNode) + 9) - 9;
+            let rect = event.target.getBoundingClientRect()
+            offsetX = (event.touches[0].clientX - window.pageXOffset - rect.left)
+            offsetY = (event.touches[0].clientY - window.pageYOffset - rect.top)
             
-            if (X % 2 == Y % 2) 
-            target.style.background = '#f06948'
-                    
-            warnCases.push(target);
+    
+            activePiece.style.position = 'absolute'
+            activePiece.style.left = `${mouseX - u}px` 
+            activePiece.style.top =`${mouseY - u}px`
+    
+            markedPiece = event.target.parentElement;
+            markedPiece.style.boxShadow = 'inset 0 0 0 3px #94d678';
+            // call function get moves
+        }
+}
+
+// warn cases
+function warnCase(event) {
+    let target = event.target;
+    if (event.target.tagName == 'DIV')
+        target = target.parentElement;
+
+    let targetIndex = Array.prototype.indexOf.call(warnCases, target);
+    if (targetIndex >= 0) {
+        warnCases.splice(targetIndex, 1);
+        target.style.background = ''
+    }
+    else {
+        target.style.background = '#db4e2c'
+
+        let X = Array.prototype.indexOf.call(target.parentNode.children, target) - 1;
+        let Y = Math.abs(Array.prototype.indexOf.call(target.parentNode.parentNode.children, target.parentNode) + 9) - 9;
+        
+        if (X % 2 == Y % 2) 
+        target.style.background = '#f06948'
+                
+        warnCases.push(target);
+    }
+}
+function stopWarn(event) {
+    if (event.target.children.length == 0 && event.target.tagName != 'DIV') {
+        // delete marker
+        while (warnCases.length > 0) {
+            warnCases[0].style.background = ''
+            warnCases.shift()
         }
     }
 }
@@ -93,16 +133,85 @@ window.onmouseup = () => {
     }
 
     activePiece = null; 
-    [currentY, currentX, startY, startX] = [0, 0, 0, 0]
+    chessboard[chessboard.length - lastMoveY - 1][lastMoveX].style.boxShadow = '';
+    [currentY, currentX, startY, startX, lastMoveY, lastMoveX] = [0, 0, 0, 0, 0, 0]
+}
+window.ontouchend = () => {
+    if (activePiece) {
+        activePiece.style.position = `initial`
+    }
+
+    // verification
+    if ((true) && activePiece) {
+        if ((currentX != startX || currentY != startY) && (chessboard.length - currentY - 1 < chessboard.length && chessboard.length - currentY - 1 >= 0) && (currentX < chessboard[chessboard.length - currentY - 1].length && currentX >= 0)) {
+            activePiece.parentElement.removeChild(activePiece);
+            chessboard[chessboard.length - currentY - 1][currentX].innerHTML = ''
+            chessboard[chessboard.length - currentY - 1][currentX].appendChild(activePiece);
+
+            if (markedPiece)
+                markedPiece.style.boxShadow =  '';
+
+            // delete marker
+            while (warnCases.length > 0) {
+                warnCases[0].style.background = ''
+                warnCases.shift()
+            }
+
+            move = !move
+        }
+    }
+
+    activePiece = null; 
+    chessboard[chessboard.length - lastMoveY - 1][lastMoveX].style.boxShadow = '';
+    [currentY, currentX, startY, startX, lastMoveY, lastMoveX] = [0, 0, 0, 0, 0, 0]
 }
 
 window.onresize = (event) => {
-    if (window.innerWidth <= 500) 
-        u = (window.innerWidth - 50) / 18;
+    if (window.innerWidth <= 750) 
+        u = (window.innerWidth - 75) / 18;
     else 
-        u = 50 / 2
+        u = 75 / 2
 }
 
+window.ontouchmove = (event) => {
+    if (activePiece != null) {
+        activePiece.style.left = `${event.touches[0].clientX - u}px` 
+        activePiece.style.top =`${event.touches[0].clientY - u}px`
+
+        indexX = (mouseX - event.touches[0].clientX - offsetX) / (u * 2)
+        if (indexX > 0)
+            indexX++;
+        indexY = (mouseY - event.touches[0].clientY - offsetY) / (u * 2)
+            if (indexY > 0)
+                indexY++;
+
+        indexX = parseInt(indexX);
+        indexY = parseInt(indexY);
+
+        let X = Array.prototype.indexOf.call(markedPiece.parentNode.children, markedPiece) - 1;
+        let Y = Math.abs(Array.prototype.indexOf.call(markedPiece.parentNode.parentNode.children, markedPiece.parentNode) + 9) - 9;
+        
+        currentY = Y - indexY;
+        currentX = X - indexX;
+
+        startX = X
+        startY = Y
+
+        if (lastMoveX != -1 && lastMoveY != -1) {
+            if (chessboard.length - lastMoveY - 1 >= 0 && chessboard.length - lastMoveY - 1 < chessboard.length)
+                if (lastMoveX >= 0 && lastMoveX < chessboard[chessboard.length - lastMoveY - 1].length)
+                    chessboard[chessboard.length - lastMoveY - 1][lastMoveX].style.boxShadow = '';
+            if (chessboard.length - currentY - 1 >= 0 && chessboard.length - currentY - 1 < chessboard.length)
+                if (currentX >= 0 && currentX < chessboard[chessboard.length - currentY - 1].length)
+                    chessboard[chessboard.length - currentY - 1][currentX].style.boxShadow = 'inset 0 0 0 3px #78acd6';
+            markedPiece.style.boxShadow = 'inset 0 0 0 3px #94d678';
+        }
+
+        lastMoveX = currentX;
+        lastMoveY = currentY;
+
+    }
+}
 window.onmousemove = (event) => {
     if (activePiece != null) {
         activePiece.style.left = `${event.clientX - u}px` 
@@ -120,13 +229,25 @@ window.onmousemove = (event) => {
 
         let X = Array.prototype.indexOf.call(markedPiece.parentNode.children, markedPiece) - 1;
         let Y = Math.abs(Array.prototype.indexOf.call(markedPiece.parentNode.parentNode.children, markedPiece.parentNode) + 9) - 9;
-
         
         currentY = Y - indexY;
         currentX = X - indexX;
 
         startX = X
         startY = Y
+
+        if (lastMoveX != -1 && lastMoveY != -1) {
+            if (chessboard.length - lastMoveY - 1 >= 0 && chessboard.length - lastMoveY - 1 < chessboard.length)
+                if (lastMoveX >= 0 && lastMoveX < chessboard[chessboard.length - lastMoveY - 1].length)
+                    chessboard[chessboard.length - lastMoveY - 1][lastMoveX].style.boxShadow = '';
+            if (chessboard.length - currentY - 1 >= 0 && chessboard.length - currentY - 1 < chessboard.length)
+                if (currentX >= 0 && currentX < chessboard[chessboard.length - currentY - 1].length)
+                    chessboard[chessboard.length - currentY - 1][currentX].style.boxShadow = 'inset 0 0 0 3px #78acd6';
+            markedPiece.style.boxShadow = 'inset 0 0 0 3px #94d678';
+        }
+
+        lastMoveX = currentX;
+        lastMoveY = currentY;
 
         // LOG IN CASE OF PROBLEMS
         //console.log(startX, startY)
@@ -135,10 +256,10 @@ window.onmousemove = (event) => {
 }
 
 window.onload = () => {
-    if (window.innerWidth <= 500) 
-        u = (window.innerWidth - 50) / 18;
+    if (window.innerWidth <= 750) 
+        u = (window.innerWidth - 75) / 18;
     else 
-        u = 50 / 2
+        u = 75 / 2
 
     // init 
     let chessboardtable = document.querySelector('tbody');
@@ -146,13 +267,16 @@ window.onload = () => {
 
     chessboardtable.querySelectorAll('th').forEach(e => {
         e.addEventListener('mousedown', (event) => { if (event.preventDefault) event.preventDefault() })
+        e.addEventListener('touchstart', (event) => { if (event.preventDefault) event.preventDefault() })
     })
     chessboardtable.querySelectorAll('td').forEach(e => {
         e.addEventListener('mousedown', (event) => { if (event.preventDefault) event.preventDefault() })
+        e.addEventListener('touchstart', (event) => { if (event.preventDefault) event.preventDefault() })
     })
 
     for (let i = 0; i < pieces.length; i++) {
         pieces[i].addEventListener('mousedown', (event) => { focusPiece(event) })
+        pieces[i].addEventListener('touchstart', (event) => { mobileFocusPiece(event) })
     }
 
     for (let i = 0; i < chessboardtable.children.length - 1; i++) {
@@ -160,6 +284,7 @@ window.onload = () => {
         for (let j = 1; j < chessboardtable.children[i].children.length; j++) {
             chessboard[chessboard.length - 1].push(chessboardtable.children[i].children[j])
             chessboardtable.children[i].children[j].addEventListener('contextmenu', (event) => { event.preventDefault(); warnCase(event) })
+            chessboardtable.children[i].children[j].addEventListener('click', (event) => { stopWarn(event) })
         }
     }
 
